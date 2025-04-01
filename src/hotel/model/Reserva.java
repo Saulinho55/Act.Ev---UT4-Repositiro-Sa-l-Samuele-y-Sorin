861e82d88bd4956fe9b0a4d34ae0bd124dea8c34
Saulinho55 <Saulespino555@gmail.com>
2025-04-01 12:17:48 +0100

    Actualizacion de Modelos
    Modelos acabados o casi terminados

---
+++ src/hotel/model/Reserva.java
@@ -0,0 +1,77 @@
+package hotel.model;
+import java.time.LocalDate;
+import java.util.List;
+
+public class Reserva {
+    private int id;
+    private Habitacion habitacion;
+    private Cliente cliente;
+    private LocalDate CheckIn;
+    private LocalDate CheckOut;
+    private double PrecioTotal;
+
+    public Reserva(int id, Habitacion habitacion, Cliente cliente, LocalDate checkIn, LocalDate checkOut) {
+        this.id = id;
+        this.habitacion = habitacion;
+        this.cliente = cliente;
+        this.CheckIn = checkIn;
+        this.CheckOut = checkOut;
+        this.PrecioTotal = PrecioTotal();
+    }
+
+    private double PrecioTotal() {
+        long dias = CheckIn.until(CheckOut).getDays();
+        if (dias > 90) {
+            System.out.println("Error. No puede exceder 90 dias");
+        }
+        PrecioTotal = dias * habitacion.getPrecioNoche();
+        return PrecioTotal;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Habitacion getHabitacion() {
+        return habitacion;
+    }
+
+    public void setHabitacion(Habitacion habitacion) {
+        this.habitacion = habitacion;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public LocalDate getCheckIn() {
+        return CheckIn;
+    }
+
+    public void setCheckIn(LocalDate checkIn) {
+        CheckIn = checkIn;
+    }
+
+    public LocalDate getCheckOut() {
+        return CheckOut;
+    }
+
+    public void setCheckOut(LocalDate checkOut) {
+        CheckOut = checkOut;
+    }
+
+    public double getPrecioTotal() {
+        return PrecioTotal;
+    }
+
+    public void setPrecioTotal(double precioTotal) {
+        PrecioTotal = precioTotal;
+    }
+}
